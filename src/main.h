e0bb550df9885a22370ab2b003bbe58b92968f25
Jonas Gehring <jonas.gehring@boolsoft.org>
2008-10-04 22:11:22 +0000

    Changed version number

--- src/main.h
+++ src/main.h
@@ -28,7 +28,7 @@
 /* Application-specific constants */
 #ifndef HAVE_CONFIG_H
  #define APPNAME "rsvndump"
- #define APPVERSION "0.4"
+ #define APPVERSION "0.4.1"
  #define APPAUTHOR "Jonas Gehring <jonas.gehring@boolsoft.org>"
 #else
  #include "config.h"
