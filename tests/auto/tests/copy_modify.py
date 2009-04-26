2610de3b4f3923875a7a3f7d6dfee195ba84f1a2
Jonas Gehring <jonas.gehring@boolsoft.org>
2009-04-26 22:49:33 +0200

    Added more tests

---
+++ tests/auto/tests/copy_modify.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+#	rsvndump test suite
+#
+
+
+import os
+
+from run import run
+
+
+def info():
+	return "Copying test with modifcations in the same revision"
+
+
+def modify_tree(step, logfile):
+	if step == 0:
+		os.mkdir("dir1")
+		os.mkdir("dir1/sdir1")
+		os.mkdir("dir1/sdir2")
+		f = open("dir1/file1",'w')
+		print >>f, 'hello1'
+		f = open("dir1/sdir1/file1",'w')
+		print >>f, 'hello2'
+		f = open("dir1/sdir2/file1",'w')
+		print >>f, 'hello3'
+		f = open("dir1/sdir2/file2",'w')
+		print >>f, 'hello4'
+		run("svn", "add", "dir1", output=logfile)
+		return 1
+	elif step == 1:
+		f = open("dir1/sdir2/file2",'a')
+		print >>f, 'hello5'
+		return 1
+	elif step == 2:
+		f = open("dir1/sdir1/file2",'w')
+		print >>f, 'hello6'
+		run("svn", "add", "dir1/sdir1/file2", output=logfile)
+		return 1
+	elif step == 3:
+		run("svn", "cp", "dir1", "dir2", output=logfile)
+		f = open("dir2/sdir2/file1",'a')
+		print >>f, "just modified!"
+		return 1
+	elif step == 4:
+		f = open("dir2/sdir2/file2",'a')
+		print >>f, "just copied!"
+		return 1
+
