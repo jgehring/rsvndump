7b7644e4736689cb7da3bf83a2f6fe66f5ea8d85
Jonas Gehring <jonas.gehring@boolsoft.org>
2009-05-10 03:16:37 +0200

    Added test for subdirectory copy & replace

---
+++ tests/db/tests/sub_copy_replace.py
@@ -0,0 +1,86 @@
+#
+#	Test database for rsvndump
+#	written by Jonas Gehring
+#
+
+
+import os, shutil
+
+import test_api
+
+
+def info():
+	return "Copying test with replacement in the same revision [sub-directory]"
+
+
+def setup(step, log):
+	if step == 0:
+		os.mkdir("dir1")
+		os.mkdir("dir1/sdir1")
+		os.mkdir("dir1/sdir2")
+		f = open("dir1/file1","w")
+		print >>f, "hello1"
+		f = open("dir1/sdir1/file1","w")
+		print >>f, "hello2"
+		f = open("dir1/sdir2/file1","w")
+		print >>f, "hello3"
+		f = open("dir1/sdir2/file2","w")
+		print >>f, "hello4"
+		test_api.run("svn", "add", "dir1", output = log)
+		return True
+	elif step == 1:
+		f = open("dir1/sdir1/file2","w")
+		print >>f, "hello6"
+		test_api.run("svn", "add", "dir1/sdir1/file2", output = log)
+		return True
+	elif step == 2:
+		os.chdir("dir1/sdir1")
+		f = open("file1","a")
+		print >>f, "hello7"
+		return True
+	elif step == 3:
+		os.chdir("..")
+		test_api.run("svn", "up", output = log)
+		test_api.run("svn", "cp", "sdir2", "sdir3", output = log)
+		return True
+	elif step == 4:
+		os.chdir("..")
+		f = open("dir1/sdir3/file2","a")
+		print >>f, "just copied!"
+		return True
+	else:
+		return False
+
+
+# Runs the test
+def run(id, args = []):
+	# Set up the test repository
+	test_api.setup_repos(id, setup)
+
+	odump_path = test_api.dump_original(id)
+	rdump_path = test_api.dump_rsvndump_sub(id, "dir1", args)
+	tmp = test_api.dump_reload(id, rdump_path)
+
+	vdump_path = test_api.data_dir()+"/"+test_api.name(id)+".dump"
+	if "--deltas" in args:
+		vdump_path += ".deltas"
+	if "--keep-revnums" in args:
+		vdump_path += ".keep_revnums"
+
+	# We need to strip the date property from the dumpfile generated
+	# by rsvndump in order to validate it properly
+	tmp = test_api.mktemp(id)
+	o = open(tmp, "w")
+	cnt = -1
+	for line in open(rdump_path):
+		if line == "svn:date\n":
+			cnt = 2
+		elif cnt > 0:
+			cnt -= 1
+			if cnt == 0:
+				continue
+		o.write(line)
+	o.close()
+	shutil.move(tmp, rdump_path)
+
+	return test_api.diff(id, rdump_path, vdump_path)
