60016606da922e0418ebfd6f97f0b6d14babe341
Jonas Gehring <jonas.gehring@boolsoft.org>
2009-07-11 16:45:08 +0200

    Updated build system

--- src/main.h
+++ src/main.h
@@ -46,6 +46,9 @@
 	#ifndef HAVE_MEMCMP
 		extern int *memcmp(const void *b1, const void *b2, size_t len);
 	#endif
+	#ifndef HAVE_STRDUP
+		extern char *strdup(const char *s);
+	#endif
 
 	/* Internationalization */
 	#if ENABLE_NLS
