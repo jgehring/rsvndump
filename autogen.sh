d6418e002648bbbe7c7896298bb8794ba6778585
Sebastian Pipping <sebastian@pipping.org>
2022-09-13 15:40:44 +0200

    autogen.sh: Drop header as suggested

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-#
-#	A small script to invoke autotools.
-#
-#	Please note that this file is (of course) NOT licensed under the GPL,
-#	as this would make absolutely no sense for 4 lines of sh code.
-#	Therfore, it is released into the public domain.
-#
-
 exec autoreconf --warnings=all --install --verbose "$@"
